c4fa6d9105191721039a77f681ad89d2e988e4bf
Clyde Tedrick <clyde.tedrick@tealium.com>
2023-07-13 17:55:42 -0400

    going local

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,31 @@
 provider "aws" {
-  region = var.base_region
+  region = var.region
 }
 
+data "aws_vpc" "this" {
+  tags = {
+    ops_lookup_vpc = "terraform_managed"
+  }
+}
+
+# lookup vpc id
 resource "aws_route53_zone" "container_registry" {
-  name = "container.com."
+  name = "registry.tlium.local."
+
+  vpc {
+    vpc_id = data.aws_vpc.this.id
+  }
 }
 
+# get list of supported regions from ps "/common/supported_regions"
 # iterate over all regions
 resource "aws_route53_record" "region_record" {
-  for_each = { for region in var.regions : region => region }
   zone_id = aws_route53_zone.container_registry.zone_id
-  name    = "reg.container.com"
+  name    = "container.registry.tlium.local"
   type    = "CNAME"
   ttl     = "300"
 
-  set_identifier = "${each.key}-${var.account_alias}-local-reg"
-
-  latency_routing_policy {
-    region = each.value
-  }
-
   records = [
-    "registry-${each.key}.${var.account_alias}.tlium.com"
+    "registry-${var.region}.${var.account_name}.tlium.com"
   ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,60 @@
-variable "base_region" {
-  type = string
-  default = "us-east-1"
-}
-
-variable "account_alias" {
+variable "account_name" {
   type = string
   default = "pem"
 }
 
-variable "regions" {
-  type = list
-  default = [
-    "us-east-1",
-    "us-west-2",
-  ]
+variable "region" {
+  type = string
+  default = "us-west-2"
 }
+
+# Standard variables filled in using matching TF_VAR_* environment variables in the shared lib
+# variable "account_id" {
+#   type        = string
+#   description = "The numeric AWS account id to deploy in, this is a string to handle some accounts starting with zero."
+
+#   validation {
+#     condition     = length(regex("^(?P<id>[\\d]{12})$", var.account_id).id) == 12
+#     error_message = "The account_id value must be a valid 12 digit AWS account ID."
+#   }
+# }
+
+# variable "account_name" {
+#   type        = string
+#   description = "The short AWS account name to deploy in."
+# }
+
+# variable "component" {
+#   type        = string
+#   description = "The compoent name this is for."
+# }
+
+# variable "environment" {
+#   type        = string
+#   description = "The environment this is running in."
+# }
+
+# variable "environment_type" {
+#   type        = string
+#   description = "The environment type this is running in, only one of [prod, qa, dev]."
+
+#   validation {
+#     condition     = contains(["dev", "qa", "prod"], var.environment_type)
+#     error_message = "The environment_type must only be one of [dev, qa, prod]."
+#   }
+# }
+
+# variable "platform_name" {
+#   type        = string
+#   description = "The platform name to deploy in."
+# }
+
+# variable "region" {
+#   type        = string
+#   description = "The region to deploy in."
+# }
+
+# variable "jenkins_region" {
+#   type        = string
+#   description = "The local region where Jenkins is running."
+# }
